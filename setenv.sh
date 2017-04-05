77ae7ff3785489738365433f25bf9e87eea3202b
jk28189 <cootve1123@naver.com>
2017-04-05 16:00:03 +0900

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,5 @@
 ########### JAVA_HOME for Tomcat SETTING#########
-export JAVA_HOME=/usr/java/java8_64
+#export JAVA_HOME=/usr/java/java8_64
 
 ########################## APM PINPOINT Setting ########################################
 #export PINPOINT_AGENT_PATH=/usr/local/pinpoint-agent
@@ -7,25 +7,26 @@ export JAVA_HOME=/usr/java/java8_64
 #export CATALINA_OPTS="$CATALINA_OPTS -Dpinpoint.agentId=apm-agent"
 #export CATALINA_OPTS="$CATALINA_OPTS -Dpinpoint.applicationName=erpdev-apm"
 
-CATALINA_OPTS="${CATALINA_OPTS} -Dlogback.configurationFile=${CATALINA_HOME}/conf/logback.xml"
+#CATALINA_OPTS="${CATALINA_OPTS} -Dlogback.configurationFile=${CATALINA_HOME}/conf/logback.xml"
 #CATALINA_OPTS="$CATALINA_OPTS -Djava.rmi.server.hostname=xxx.xxx.xxx.xxx"
-export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/usr/local/apr/lib'
+#export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/usr/local/apr/lib'
  
 
 #log directory
 SERVER_NAME="tomcat"
-LOG_HOME="/home/lab/tomcat/logs"
-GC_LOG_HOME="${LOG_HOME}/gclog"
+#LOG_HOME="/home/lab/tomcat/logs"
+LOG_HOME="/usr/local/tomcat/logs"
+#GC_LOG_HOME="${LOG_HOME}/gclog"
 
 #log file
-DATE_TIME=`date +'%y%m%d'`
+#DATE_TIME=`date +'%y%m%d'`
 #DATE_TIME=`%d{yyyyMMdd}`
-GC_LOG_FILENM="gclog.${DATE_TIME}"
+#GC_LOG_FILENM="gclog.${DATE_TIME}"
 
 JAVA_OPTS="${JAVA_OPTS} -server"
 JAVA_OPTS="${JAVA_OPTS} -D${SERVER_NAME}"
 JAVA_OPTS="${JAVA_OPTS} -Xms256m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError"
 JAVA_OPTS="${JAVA_OPTS} -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC"
-JAVA_OPTS="${JAVA_OPTS} -Djava.security.egd=file:///dev/urandom"
-JAVA_OPTS="${JAVA_OPTS} -verbosegc -Xloggc:${GC_LOG_HOME}/${GC_LOG_FILENM}"
+#JAVA_OPTS="${JAVA_OPTS} -Djava.security.egd=file:///dev/urandom"
+#JAVA_OPTS="${JAVA_OPTS} -verbosegc -Xloggc:${GC_LOG_HOME}/${GC_LOG_FILENM}"
 export JAVA_OPTS CATALINA_OPTS
